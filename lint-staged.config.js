9820904de00107e35d2649ac9c282d631e1f52e3
lequan12 <lequan12@tebprint.com>
2023-11-08 23:38:27 +0700

    feat: add husky linstage commitlint cspell

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '*.ts': ['eslint --cache --fix'],
+  '*.tsx': ['eslint --cache --fix'],
+  '*.*': ['cspell --cache --no-summary --no-progress --no-must-find-files'],
+  // '**/*.ts?(x)': 'tsc-files --noEmit',
+  '*.json': ['prettier --write'],
+};
